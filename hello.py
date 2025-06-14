f9b24a0244f01649e0b6e88e3ca2f63fe64e39b6
Asish-Ray <aray19069@gmail.com>
2025-06-14 23:53:10 +0530

    updated python

--- hello.py
+++ hello.py
@@ -1,2 +1,25 @@
 print("hello bro")
 print("idk")
+print("this is updated")[33mcommit 6a6336d705426e9f8af43803bbf7953d8712fa41[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: Asish-Ray <aray19069@gmail.com>
+Date:   Sat Jun 14 23:46:27 2025 +0530
+
+    added hello.py
+
+[33mcommit 9ff0b06c5270aa6204720ed6a5fbb9468199e37b[m
+Author: Asish-Ray <aray19069@gmail.com>
+Date:   Fri Jun 13 23:04:17 2025 +0530
+
+    done
+
+[33mcommit 43dca1472e644a89f4251d138cb602abd07535a8[m
+Author: Asish-Ray <aray19069@gmail.com>
+Date:   Wed Jun 11 13:27:28 2025 +0530
+
+    done with gitignore
+
+[33mcommit 3f1200710522986f178b2e1dfc190a3444a2194a[m
+Author: Asish-Ray <aray19069@gmail.com>
+Date:   Tue Jun 10 06:17:02 2025 +0530
+
+    create the index.html file
